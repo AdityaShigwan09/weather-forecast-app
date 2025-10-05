feba1cff7dc0e2ae6d55aed8ac7c31b58ba2e373
Vishwanath111111 <158135422+Vishwanath111111@users.noreply.github.com>
2025-10-05 11:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.markdown("""
     /* Remove Streamlit default styling */
     .stApp {
         background-color: white !important;
-        text-color: black;
+        color: black;
         
     }
     
